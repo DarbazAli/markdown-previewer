20e19811f34ae01492f3a96bd0ba2d86e7a4b408
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-07 13:40:40 +0300

    fix style issue

--- src/Editor.js
+++ src/Editor.js
@@ -1,13 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 
 export default function Editor(props) {
     return (
-       <div>
             <textarea
                 id="editor"
                 onChange={props.handleChange}
                 defaultValue={props.value}
                 />
-       </div>    
     )
 }
+
